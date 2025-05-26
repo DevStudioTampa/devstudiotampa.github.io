cfa8faa5c21c520635502c2ff318324dc7078b41
Joey Castro <devstudiotampa@gmail.com>
2025-05-26 02:52:36 -0400

    Fix: Header case sensitivity for Vercel

--- src/DeveloperStudioLayout.jsx
+++ src/DeveloperStudioLayout.jsx
@@ -1,6 +1,6 @@
 // src/DeveloperStudioLayout.jsx
 import React from "react";
-import Header from "./Header";
+import Header from "./Header.jsx";
 
 export default function DeveloperStudioLayout() {
   return (
